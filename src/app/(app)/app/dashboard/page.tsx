fb6ebc5e091e11b6fa094c885ac9416f1ad4a061
Miguel Bravo <miguelbravobalestrini@gmail.com>
2024-05-16 10:18:59 +0100

    pet list set-up

--- src/app/(app)/app/dashboard/page.tsx
+++ src/app/(app)/app/dashboard/page.tsx
@@ -5,7 +5,13 @@ import PetList from '@/components/pet-list';
 import SearchForm from '@/components/search-form';
 import Stats from '@/components/stats';
 
-const Page = () => {
+const Page = async () => {
+  const response = await fetch('https://bytegrad.com/course-assets/projects/petsoft/api/pets');
+  if (!response.ok) {
+    throw new Error('Could not fetch pets');
+  }
+  const data = await response.json();
+
   return (
     <main>
       <div className="flex items-center justify-between text-white py-8">
@@ -13,18 +19,18 @@ const Page = () => {
 
         <Stats />
       </div>
-      <div className="grid grid-cols-3 grid-rows-[45px_1fr] gap-4 h-[600px]">
-        <div className="row-start-1 row-span-1 col-start-1 col-span-1">
+      <div className="grid grid-rows-[45px_300px_500px] md:grid-cols-3 md:grid-rows-[45px_1fr] gap-4 md:h-[600px]">
+        <div className="md:row-start-1 md:row-span-1 md:col-start-1 md:col-span-1">
           <SearchForm />
         </div>
 
-        <div className='row-start-2 row-span-full col-start-1 col-span-1'>
+        <div className='md:row-start-2 md:row-span-full md:col-start-1 col-span-1'>
           <ContentBlock>
-            <PetList />
+            <PetList pets={data}/>
           </ContentBlock>
         </div>
 
-        <div className='row-start-1 row-span-full col-start-2 col-span-full'>
+        <div className='md:row-start-1 md:row-span-full md:col-start-2 md:col-span-full'>
           <ContentBlock>
             <PetDetails />
           </ContentBlock>
--- src/components/pet-list.tsx
+++ src/components/pet-list.tsx
@@ -1,11 +1,30 @@
-const PetList = () => {
-  return (
-    <ul className="bg0white border-b border-black/[0.08]">
-      <li>
-        <button>Benjamin</button>
-      </li>
-    </ul>
-  )
+import Image from 'next/image';
+
+import { Pet } from '@/lib/types';
+
+type PetListProps = {
+  pets: Pet[];
 }
 
-export default PetList
+const PetList = ({ pets }: PetListProps) => {
+  return (
+    <ul className="bg0white border-b border-black/[0.08]">
+      {pets.map((pet) => (
+        <li key={pet.id}>
+          <button className="flex items-center h-[70px] w-full cursor-pointer px-5 text-base gap-3 hover:bg-[#EFF1F2] focus:bg-[#EFF1F2] transition">
+            <Image
+              src={pet.imageUrl}
+              alt="Pet image"
+              width={45}
+              height={45}
+              className="w-[45px] h-[45px] rounded-full object-cover"
+            />
+          </button>
+          <p className="font-semibold">{pet.name}</p>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default PetList;
