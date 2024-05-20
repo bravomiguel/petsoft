d0b38ed92adf601fdc4ecb99baaad3887651a612
Miguel Bravo <miguelbravobalestrini@gmail.com>
2024-05-20 12:59:12 +0100

    pet-form fields initial set-up

--- src/components/pet-button.tsx
+++ src/components/pet-button.tsx
@@ -48,7 +48,7 @@ export default function PetButton({
             {actionType === 'add' ? 'Add a new pet' : 'Edit pet'}
           </DialogTitle>
         </DialogHeader>
-        <PetForm />
+        <PetForm actionType={actionType} />
       </DialogContent>
     </Dialog>
   );
--- src/contexts/pet-context-provider.tsx
+++ src/contexts/pet-context-provider.tsx
@@ -38,6 +38,10 @@ export default function PetContextProvider({
     setSelectedPetId(null);
   };
 
+  const handleAddPet = (newPet: Pet) => {
+    setPets(prev => [...prev, newPet]);
+  }
+
   return (
     <PetContext.Provider
       value={{
