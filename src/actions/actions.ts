505b41b329f248ae243b143687d1575cd79a482f
Miguel Bravo <miguelbravobalestrini@gmail.com>
2024-06-01 19:53:22 -0500

    logout setup

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -5,7 +5,7 @@ import { revalidatePath } from 'next/cache';
 import prisma from '@/lib/db';
 import { sleep } from '@/lib/utils';
 import { petFormSchema, petIdSchema } from '@/lib/validations';
-import { signIn } from '@/lib/auth';
+import { signIn, signOut } from '@/lib/auth';
 
 // --- user actions ---
 
@@ -13,6 +13,10 @@ export async function logIn(formData: FormData) {
   const authData = Object.fromEntries(formData.entries());
 
   await signIn('credentials', authData);
+}
+
+export async function logOut() {
+  await signOut({ redirectTo: '/' });
 }
 
 // --- pet actions ---
--- src/app/(app)/app/account/page.tsx
+++ src/app/(app)/app/account/page.tsx
@@ -3,19 +3,21 @@ import { redirect } from 'next/navigation';
 import ContentBlock from '@/components/content-block';
 import H1 from '@/components/h1';
 import { auth } from '@/lib/auth';
+import SignOutBtn from '@/components/sign-out-btn';
 
 export default async function Page () {
   const session = await auth();
   if (!session?.user) {
     redirect("/login");
   }
-  
+
   return (
     <main>
       <H1 className="my-8 text-white">Your Account</H1>
 
-      <ContentBlock className="h-[500px] flex justify-center items-center">
+      <ContentBlock className="h-[500px] flex flex-col gap-3 justify-center items-center">
         <p>Logged in as {session.user?.email}</p>
+        <SignOutBtn />
       </ContentBlock>
     </main>
   );
